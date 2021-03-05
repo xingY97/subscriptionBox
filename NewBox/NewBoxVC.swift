67cab0ee969f6fb08ee43450f7f79b6a26183a04
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-03-05 16:54:57 -0500

    minor change

--- NewBox/NewBoxVC.swift
+++ NewBox/NewBoxVC.swift
@@ -13,7 +13,7 @@ class NewBoxVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "NewBox"
+        self.title = "NEWBOX"
 
         
     }
